9c91911dc7b4b583f28b57d100c40a78e3bd8950
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-02 11:47:51 +0530

    create dummy register route for registering user

---
+++ server/Routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+//middlewares
+
+//controllers
+
+//routes
+router.post("/register", (req, res) => {
+	console.log("Hello, RegisterApi ");
+});
+
+module.exports = router;
