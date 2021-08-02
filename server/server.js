748c47c4534e58f1c0e6aa5be9e4cd88a48919bc
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-02 11:04:04 +0530

    setUp Sever and connected DB

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+//configure  env files
+require("dotenv").config();
+
+//congigure app
+const app = express();
+
+//connect MongoDB
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		userCreateIndex: true,
+		useFindAndModify: true,
+	})
+	.then(() => console.log("DB Conected"))
+	.catch((err) => console.log("DB Connecton Error"));
+
+app.listen(process.env.PORT, () => {
+	console.log(`App Running on ${process.env.PORT}`);
+	console.log(`MONGO URI ON ${process.env.MONGO_URI}`);
+});
