c0228066c03e4aedcd976ab372b258aa83356b99
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-02 12:10:06 +0530

    update : Routes/userRoutes.js
    - imported and used a dummy controller function to test working

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -5,10 +5,15 @@ const router = express.Router();
 //middlewares
 
 //controllers
+const { dummy } = require("../Controllers/registerUser");
 
 //routes
 router.post("/register", (req, res) => {
 	console.log("Hello, RegisterApi ");
+	//doactions here , move the call back function to a seperate controller folder for better structure
 });
+
+//dummy
+router.get("/dummy", dummy);
 
 module.exports = router;
