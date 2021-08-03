9b0d6a4bb294eb2c6fad06c109f1da67de6a2134
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-03 13:47:48 +0530

    update : register.js => made changes to header text in UI

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -59,7 +59,7 @@ const Register = () => {
 						alt="Workflow"
 					/>
 					<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-						Sign up to your account
+						Sign up for your account
 					</h2>
 					<p className="mt-2 text-center text-sm text-gray-600">
 						Or{" "}
