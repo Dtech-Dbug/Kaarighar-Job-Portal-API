f4d7968f2fc6a290f173971cecbbafa0e0bb2e92
ashishxcode <ashish.patel0903@gmail.com>
2021-08-03 11:49:17 +0530

    Fixed error of user creation

--- server/Controllers/registerUser.js
+++ server/Controllers/registerUser.js
@@ -1,15 +1,15 @@
-const UserModel = require("../Model/user");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const UserModel = require('../Model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 exports.dummy = async (req, res) => {
-	console.log("Dummy woute getting request");
+	console.log('Dummy woute getting request');
 };
 
 exports.registerUser = async (req, res) => {
-	console.log("Hello, RegisterApi ");
+	console.log('Hello, RegisterApi ');
 	//doactions here , move the call back function to a seperate controller folder for better structure
-
+	console.log('req.body: ', req.body);
 	const {
 		firstName,
 		lastName,
@@ -21,7 +21,7 @@ exports.registerUser = async (req, res) => {
 		address,
 		pincode,
 		hometown,
-	} = req.body;
+	} = req.body.values;
 
 	//@desc: Creating user using register route
 	//@input: Registet from data
@@ -34,7 +34,7 @@ exports.registerUser = async (req, res) => {
 			return res
 				.status(400)
 				.status(400)
-				.json({ errros: [{ msg: "User already exits" }] });
+				.json({ errros: [{ msg: 'User already exits' }] });
 		}
 
 		// Creating user object
@@ -52,10 +52,10 @@ exports.registerUser = async (req, res) => {
 		});
 
 		// Creating the secret key for the JWT
-		const salt = await bcrypt.genSalt(10);
+		// const salt = await bcrypt.genSalt(10);
 
 		// Hashing the password
-		user.password = await bcrypt.hash(password, salt);
+		// user.password = await bcrypt.hash(password, salt);
 
 		// Saving the user
 		await user.save();
@@ -70,15 +70,15 @@ exports.registerUser = async (req, res) => {
 		jwt.sign(
 			payload,
 			process.env.JWT_SECRET,
-			{ expiresIn: "5 days" },
+			{ expiresIn: '5 days' },
 			(err, token) => {
 				if (err) throw err;
 				res.json({ token });
-			}
+			},
 		);
 	} catch (err) {
 		console.error(err.message);
-		console.log("ERROR WHILE RESGISTERING__>", err);
-		res.status(500).send("Server error");
+		console.log('ERROR WHILE RESGISTERING__>', err);
+		res.status(500).send('Server error');
 	}
 };
