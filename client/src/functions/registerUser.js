352e0cebf5e3011e4ec2f76ca26e5b346b3dcfc9
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-03 19:32:05 +0530

    created the frontend function to make request to backend to logIn user

--- client/src/functions/registerUser.js
+++ client/src/functions/registerUser.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export const registerUser = async (values) => {
 	return await axios.post("http://localhost:8000/api/register", { values });
 };
+
+export const loginUser = async (values) => {
+	return await axios.post("http://localhost:8000/api/login");
+};
