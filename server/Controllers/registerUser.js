0f0af0eb8e5711840143b2e9624eb952ec6e3f22
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-03 21:35:26 +0530

    ...

--- server/Controllers/registerUser.js
+++ server/Controllers/registerUser.js
@@ -1,17 +1,17 @@
-const UserModel = require('../Model/user');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const UserModel = require("../Model/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.dummy = async (req, res) => {
-	console.log('Dummy woute getting request');
+	console.log("Dummy woute getting request");
 };
 
 exports.registerUser = async (req, res) => {
-	console.log('Hello, RegisterApi ');
+	console.log("Hello, RegisterApi ");
 	console.log(req.body);
 
 	//doactions here , move the call back function to a seperate controller folder for better structure
-	console.log('req.body: ', req.body);
+	console.log("req.body: ", req.body);
 
 	const {
 		firstName,
@@ -35,7 +35,7 @@ exports.registerUser = async (req, res) => {
 	try {
 		let user = await UserModel.findOne({ mobileNumber });
 
-		console.log('USER--->', user);
+		console.log("USER--->", user);
 
 		if (user) {
 			return res.json({ ok: true });
@@ -61,11 +61,11 @@ exports.registerUser = async (req, res) => {
 
 		// Hashing the password
 		user.password = await bcrypt.hash(password, salt);
-		console.log('PASSWORD HASHED--->', user.password);
+		console.log("PASSWORD HASHED--->", user.password);
 
 		//save user
 		const userSaved = await user.save();
-		console.log('USER --->', userSaved);
+		console.log("USER ____>", userSaved);
 
 		const payload = {
 			user: {
@@ -78,80 +78,36 @@ exports.registerUser = async (req, res) => {
 		jwt.sign(
 			payload,
 			process.env.JWT_SECRET,
-			{ expiresIn: '5 days' },
+			{ expiresIn: "5 days" },
 			(err, token) => {
 				if (err) throw err;
 				res.json({ token });
-			},
+			}
 		);
 	} catch (err) {
 		console.error(err.message);
-		console.log('ERROR WHILE RESGISTERING__>', err);
-		res.status(500).send('Server error');
+		console.log("ERROR WHILE RESGISTERING__>", err);
+		res.status(500).send("Server error");
 	}
 };
 
 exports.loginUser = async (req, res) => {
-	try {
-		console.log('req -> body --->', req.body);
-		//step 1
-		//get the logIn details from frontend
-		// let { mobileNumber, password } = req.body.values;
+	//step 1
+	//get the logIn details from frontend
+	const { mobileNumber, password } = req.body.values;
 
-		const mobileNumber = 8758549166;
-		const password = 'qwerty123';
+	//step2
+	//query the databse using the mobileNo to find the user
 
-		console.log('mobileNumber: ', mobileNumber);
-		console.log('password: ', password);
+	const user = await UserModel.findOne(mobileNumber).exec();
 
-		//step2
-		//query the databse using the mobileNo to find the user
-		let user = await UserModel.findOne({ mobileNumber });
+	//destructure mobile and password from user database
+	const { mobileNumber, password } = user;
+	//step3
+	//compare the req.body.password with the password in the database
 
-		// step 3
-		// checking if user exists or not
-		if (!user) {
-			// return res
-			// 	.status(400)
-			// 	.json({ errors: [{ msg: 'Invalid Credentials' }] });
-			console.log('Inavalid Credentials');
-		}
-
-		//step 4
-		//compare the req.body.password with the password in the database
-		const isMatch = await bcrypt.compare(password, user.password);
-
-		//step 5
-		//if password not matched then return error
-		if (!isMatch) {
-			return res.status(400).json('Mobile No. or Password is incorrect');
-		}
-
-		// step 6
-		//if password matched then create the token
-
-		// creating payload for the JWT
-		const payload = {
-			user: {
-				id: user.id,
-			},
-		};
-
-		// step 7
-		// Creating the JWT token
-		const token = jwt.sign(
-			payload,
-			process.env.JWT_SECRET,
-			{ expiresIn: '5 days' },
-			(err, token) => {
-				if (err) throw err;
-				res.json({ token });
-				console.log('TOKEN: ', res.json({ token }));
-			},
-		);
-	} catch (err) {
-		console.error(err.message);
-		console.log('ERROR WHILE LOGGING IN__>', err);
-		res.status(500).send('Server error');
-	}
+	//step4
+	//send ok response and login user if the comparison matches
+	//else , show error
+	//BRO i am on a call, I have not left discord
 };
