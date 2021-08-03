a8ea1a19c031eb188523867b66df765c42c4e8a6
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-03 10:57:03 +0530

    update : userRoutes: removed redundant codes

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const UserModel = require("../Model/user");
-
 //middlewares
 
 //controllers
