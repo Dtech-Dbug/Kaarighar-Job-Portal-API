065f72c2b0278744a79d1ebbeb09ab661e649610
ashishxcode <ashish.patel0903@gmail.com>
2021-08-04 21:45:46 +0530

    Create dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Dashboard from './components/dashboard/Dashboard';
 function App() {
 	return (
 		<div className="App">
 			<Router>
 				<Switch>
-					<Route exact path="/" component={Home} />
+					<Route exact path="/" component={Dashboard} />
 					<Route path="/register" component={Register} />
 					<Route path="/login" component={Login} />
 				</Switch>
