a13d3535c9d7ba3a0e08ff3840567491a1f5acd7
ashishxcode <ashish.patel0903@gmail.com>
2021-08-05 20:19:13 +0530

    Created Users component

---
+++ client/src/components/users/Users.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { verifyUser, getAllUsers } from '../../functions/users';
+
+const Users = () => {
+	const [usersList, setUsersList] = useState([]);
+	const [jobSeekerList, setJobSeekerList] = useState([]);
+	const [recruiterList, setRecruiterList] = useState([]);
+
+	useEffect(() => {
+		getAllUsers().then((res) => setUsersList(res.data));
+
+		console.log(usersList);t
+		if (usersList.length) {
+			const jobSeekers = usersList.filter(
+				(user) => user.role === 'Job Seeker',
+			);
+
+			const recruiters = usersList.filter(
+				(user) => user.role === 'Recruiter',
+			);
+			setJobSeekerList(jobSeekers);
+			setRecruiterList(recruiters);
+		}
+	}, []);
+	return (
+		<div className="text-left">
+			<h1 className="text-xl">Users</h1>
+			<div>
+				{jobSeekerList.length
+					? console.log('Job Skeers =>', jobSeekerList)
+					: 'null'}
+			</div>
+			<div>
+				{recruiterList.length
+					? console.log('Recruiters =>', recruiterList)
+					: 'null'}
+			</div>
+		</div>
+	);
+};
+
+export default Users;
