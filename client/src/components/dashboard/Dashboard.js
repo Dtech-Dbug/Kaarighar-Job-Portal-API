9bce073107aece0b22aea4e0048690724a69d46b
ashishxcode <ashish.patel0903@gmail.com>
2021-08-05 20:18:20 +0530

    Added links in dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Dashboard = () => {
 	return (
 		<div>
 			<h1>Dashboard</h1>
+			<div className=" ">
+				<ul className="flex p-1 justify-center items-center">
+					<li className="mx-1 text-lg">
+						<Link to="/users">Users</Link>
+					</li>
+					<li className="mx-1 text-lg">
+						<Link to="/jobs">Jobs</Link>
+					</li>
+					<li className="mx-1 text-lg">
+						<Link to="/category">Category</Link>
+					</li>
+					<li className="mx-1 text-lg">
+						<Link to="/profile">Profile</Link>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
