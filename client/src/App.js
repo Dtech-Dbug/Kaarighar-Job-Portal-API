339977292280120467840415e490a9bd7f04333d
ashishxcode <ashish.patel0903@gmail.com>
2021-08-05 20:21:24 +0530

    Added Routes for dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
+import Users from './components/users/Users';
 function App() {
 	return (
 		<div className="App">
@@ -11,6 +12,7 @@ function App() {
 					<Route exact path="/" component={Dashboard} />
 					<Route path="/register" component={Register} />
 					<Route path="/login" component={Login} />
+					<Route path="/users" component={Users} />
 				</Switch>
 			</Router>
 		</div>
