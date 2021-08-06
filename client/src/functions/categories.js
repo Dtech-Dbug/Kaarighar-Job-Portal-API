3f345115822de9d7803e8c6123702d0bf650c1af
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-06 23:46:22 +0530

    update : categories.js : createCategory function to interact w/ backend
    - front end function to send the user input to backend to create category

--- client/src/functions/categories.js
+++ client/src/functions/categories.js
@@ -3,10 +3,14 @@ import axios from "axios";
 //CRUD
 const url = "http://localhost:8000/api/admin/category";
 
-export const createCategory = async (authtoken, values) => {
-	return await axios.post(url, values, {
-		headers: {
-			"auth-token": authtoken,
-		},
-	});
+export const createCategory = async (values) => {
+	return await axios.post(
+		url,
+		{ values },
+		{
+			headers: {
+				"auth-token": localStorage.getItem("token"),
+			},
+		}
+	);
 };
