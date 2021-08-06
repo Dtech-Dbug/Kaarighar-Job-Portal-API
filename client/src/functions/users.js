428007267fd06a61ad9e3fb3fd449f8bd2ba1a25
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-06 11:57:58 +0530

    passed authtoken as headers in frontend function , while making admin specific request to backend

--- client/src/functions/users.js
+++ client/src/functions/users.js
@@ -1,20 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const verifyUser = async (values) => {
-	return await axios.post('http://localhost:8000/api/admin/verifyuser', {
+	return await axios.post("http://localhost:8000/api/admin/verifyuser", {
 		values,
 		headers: {
-			'auth-token':
-				'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEwOTQ4NTE0M2UyNTk0M2Q0NDk1MjFjIiwicm9sZSI6IkpvYiBTZWVrZXIifSwiaWF0IjoxNjI4MTc0NDQzLCJleHAiOjE2Mjg2MDY0NDN9.4zIVsEBtja-QT7TEZSwMpYY5etSbpl2bpbUU2R5C6so',
+			"auth-token":
+				"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEwOTQ4NTE0M2UyNTk0M2Q0NDk1MjFjIiwicm9sZSI6IkpvYiBTZWVrZXIifSwiaWF0IjoxNjI4MTc0NDQzLCJleHAiOjE2Mjg2MDY0NDN9.4zIVsEBtja-QT7TEZSwMpYY5etSbpl2bpbUU2R5C6so",
 		},
 	});
 };
 
-export const getAllUsers = async () => {
-	return await axios.get('http://localhost:8000/api/users', {
+export const getAllUsers = async (authtoken) => {
+	return await axios.get("http://localhost:8000/api/users", {
 		headers: {
-			'auth-token':
-				'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEwOTQ4NTE0M2UyNTk0M2Q0NDk1MjFjIiwicm9sZSI6IkpvYiBTZWVrZXIifSwiaWF0IjoxNjI4MTc0NDQzLCJleHAiOjE2Mjg2MDY0NDN9.4zIVsEBtja-QT7TEZSwMpYY5etSbpl2bpbUU2R5C6so',
+			authtoken,
 		},
 	});
 };
