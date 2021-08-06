5b9a384939fb5ada37943200b4b90b6f10963954
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-07 01:59:43 +0530

    update : App.js
    - imported and used EditCategory component
    
    - removed <Switch> wrapping : was causing troubles to component rendering w/ slug

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Dashboard from './components/dashboard/Dashboard';
-import Users from './components/users/Users';
-import Home from './components/Home';
-import Category from './components/category/Category';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Dashboard from "./components/dashboard/Dashboard";
+import Users from "./components/users/Users";
+import Home from "./components/Home";
+import Category from "./components/category/Category";
+import EditCategory from "./components/category/editCategory";
 function App() {
 	return (
 		<div className="h-screen w-sreen flex overflow-hidden">
 			<Router>
-				<Switch>
-					<Route exact path="/" component={Dashboard} />
-					<Route path="/register" component={Register} />
-					<Route path="/login" component={Login} />
-					<Route path="/users" component={Users} />
-					<Route path="/home" component={Home} />
-					<Route path="/category" component={Category} />
-				</Switch>
+				<Route path="/" exact component={Dashboard} />
+				<Route path="/register" exact component={Register} />
+				<Route path="/login" exact component={Login} />
+				<Route path="/users" exact component={Users} />
+				<Route path="/home" exact component={Home} />
+				<Route path="/category" exact component={Category} />
+				<Route path="/category/:slug" exact component={EditCategory} />
 			</Router>
 		</div>
 	);
