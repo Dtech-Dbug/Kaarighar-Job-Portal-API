529368c16437ec11416afe347862e396e270676b
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-06 23:45:26 +0530

    update : categoryForm.js
    - caught the props  and attached the function w/ events on elements

--- client/src/components/category/CategoryForm.js
+++ client/src/components/category/CategoryForm.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const CategoryForm = ({ handleCategoryChange }) => {
+const CategoryForm = ({ handleCategoryChange, handleCategoryFormSubmit }) => {
+	// const handleCategoryChange = (e) => {
+	// 	// console.log(e.target.value);
+	// 	setFormData(e.target.value);
+	// 	console.log("Form data", formData);
+	// };
+
 	return (
 		<div className="max-w-md w-full space-y-8">
 			<div>
@@ -14,7 +20,8 @@ const CategoryForm = ({ handleCategoryChange }) => {
 					<div className="mb-4 mr-1">
 						<label
 							className="block text-grey-darker text-sm font-bold mb-2"
-							htmlFor="category-name">
+							htmlFor="category-name"
+						>
 							Category Name
 							<span className="text-red-900">*</span>
 						</label>
@@ -31,7 +38,8 @@ const CategoryForm = ({ handleCategoryChange }) => {
 					<div className="mb-4">
 						<label
 							className="block text-grey-darker text-sm font-bold mb-2"
-							htmlFor="category-image">
+							htmlFor="category-image"
+						>
 							Category Image
 							<span className="text-red-900">*</span>
 						</label>
@@ -47,8 +55,10 @@ const CategoryForm = ({ handleCategoryChange }) => {
 
 				<div>
 					<button
+						onClick={handleCategoryFormSubmit}
 						type="submit"
-						className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+						className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+					>
 						<span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
 						Add Category
 					</button>
