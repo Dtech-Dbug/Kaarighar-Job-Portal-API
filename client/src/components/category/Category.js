dc3c2eb2977c24c2972d892f4498de691a450909
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-06 23:44:46 +0530

    update : category.js
    - stored state of user input
    - passed handle change and handle submit functions down as props to child component
    - handle change to set state of user input
    - handle submit to send the input values to backend to create category

--- client/src/components/category/Category.js
+++ client/src/components/category/Category.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import CategoryForm from './CategoryForm';
-import CategoryList from './CategoryList';
+import React, { useState } from "react";
+import CategoryForm from "./CategoryForm";
+import CategoryList from "./CategoryList";
+import { createCategory } from "../../functions/categories";
 
 const Category = () => {
+	const [formData, setFormData] = useState("");
 	const handleCategoryChange = (e) => {
-		console.log(e.target.value);
+		// console.log(e.target.value);
+		setFormData(e.target.value);
+		console.log("Form data", formData);
+	};
+
+	const handleCategoryFormSubmit = (values) => {
+		console.log("HELLOW WORLD");
+		console.log(formData);
+
+		//making req to BE to create category
+		createCategory(formData)
+			.then((res) => alert("CATEGORY CREATED"))
+			.catch((err) => console.log(err.message));
 	};
 	return (
 		<div className="w-full flex flex-col items-center bg-gray-50  lg:px-8">
-			<CategoryForm handleCategoryChange={handleCategoryChange} />
+			<CategoryForm
+				handleCategoryChange={handleCategoryChange}
+				handleCategoryFormSubmit={handleCategoryFormSubmit}
+			/>
 			<CategoryList />
 		</div>
 	);
