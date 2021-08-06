3e51b944845aa9c0a6cd94d4ca3cefdf92078168
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-06 19:54:45 +0530

    Updated categorieschema : added images

--- server/Model/jobCategories.js
+++ server/Model/jobCategories.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
+//text field to be able to search the categories by text in the future
+
 const categoriesSchema = new Schema(
 	{
 		title: {
@@ -15,6 +17,11 @@ const categoriesSchema = new Schema(
 			trim: true,
 			lowercase: true,
 			index: true,
+			text: true,
+		},
+
+		images: {
+			type: String,
 		},
 	},
 	{ timestamps: true }
