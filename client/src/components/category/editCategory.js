32fe869e425fe718b4779dba2d7e9da66b11466d
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-07 02:15:31 +0530

    update : editCategory.js
    - stored state of categories using useState
    - seperated function for clean code

--- client/src/components/category/editCategory.js
+++ client/src/components/category/editCategory.js
@@ -1,19 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { readCategory } from "../../functions/categories";
 
 //function to fetch data of a sinle category
 
 const EditCategory = ({ match }) => {
+	const [category, setCategory] = useState();
+	const [formValue, setFormValue] = useState("check");
+
 	useEffect(() => {
-		readCategory(match.params.slug).then((res) =>
-			console.log("Categoty:", res)
-		);
+		fetchData();
 	}, []);
+
+	const fetchData = () =>
+		readCategory(match.params.slug).then((res) => {
+			console.log("Categoty:", res);
+			setCategory(res.data);
+		});
 
 	console.log("Match", match);
 	return (
 		<>
 			<h1>Edit category : {match.params.slug} </h1>
+			<br />
+			{JSON.stringify(category)}
 		</>
 	);
 };
