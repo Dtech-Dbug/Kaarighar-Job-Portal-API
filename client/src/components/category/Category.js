ad29966b64592ac5340401fbe4a30c80fc49bf41
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-07 01:36:14 +0530

    update : category/category.js
    - unnecessary routes to '#' on submit , by preventing default

--- client/src/components/category/Category.js
+++ client/src/components/category/Category.js
@@ -1,32 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import CategoryForm from './CategoryForm';
-import CategoryList from './CategoryList';
-import { createCategory, getAllCategories } from '../../functions/categories';
+import React, { useState, useEffect } from "react";
+import CategoryForm from "./CategoryForm";
+import CategoryList from "./CategoryList";
+import { createCategory, getAllCategories } from "../../functions/categories";
 
 const Category = () => {
-	const [formData, setFormData] = useState('');
+	const [formData, setFormData] = useState("");
 	const [categoryList, setCategoryList] = useState([]);
 
 	useEffect(() => {
-		getAllCategories().then((categories) => {
-			setCategoryList(categories.data);
-		});
+		getAllCategories()
+			.then((categories) => {
+				setCategoryList(categories.data);
+			})
+			.catch((err) => alert(err.message));
 	}, []);
 
 	console.log(categoryList);
 	const handleCategoryChange = (e) => {
 		// console.log(e.target.value);
 		setFormData(e.target.value);
-		console.log('Form data', formData);
+		console.log("Form data", formData);
 	};
 
-	const handleCategoryFormSubmit = (values) => {
-		console.log('HELLOW WORLD');
+	const handleCategoryFormSubmit = (e, values) => {
+		e.preventDefault();
+		console.log("HELLOW WORLD");
 		console.log(formData);
 
 		//making req to BE to create category
 		createCategory(formData)
-			.then((res) => alert('CATEGORY CREATED'))
+			.then((res) => {
+				alert("CATEGORY CREATED");
+				// window.location.reload();
+			})
 			.catch((err) => console.log(err.message));
 	};
 
