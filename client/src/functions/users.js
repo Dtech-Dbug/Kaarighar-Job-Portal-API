9fd572f132fb06a47ad371b103781a74ff4e8eef
ashishxcode <ashish.patel0903@gmail.com>
2021-08-06 20:54:52 +0530

    Changed the way of passoing auth token

--- client/src/functions/users.js
+++ client/src/functions/users.js
@@ -1,19 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const verifyUser = async (values) => {
-	return await axios.post("http://localhost:8000/api/admin/verifyuser", {
-		values,
-		headers: {
-			"auth-token":
-				"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEwOTQ4NTE0M2UyNTk0M2Q0NDk1MjFjIiwicm9sZSI6IkpvYiBTZWVrZXIifSwiaWF0IjoxNjI4MTc0NDQzLCJleHAiOjE2Mjg2MDY0NDN9.4zIVsEBtja-QT7TEZSwMpYY5etSbpl2bpbUU2R5C6so",
+	return await axios.post(
+		'http://localhost:8000/api/admin/verifyuser',
+		{
+			values,
 		},
-	});
+		{
+			headers: {
+				'Content-Type': 'application/json',
+				'auth-token': localStorage.getItem('token'),
+			},
+		},
+	);
 };
 
-export const getAllUsers = async (authtoken) => {
-	return await axios.get("http://localhost:8000/api/users", {
+export const getAllUsers = async () => {
+	return await axios.get('http://localhost:8000/api/users', {
 		headers: {
-			authtoken,
+			'auth-token': localStorage.getItem('token'),
 		},
 	});
 };
