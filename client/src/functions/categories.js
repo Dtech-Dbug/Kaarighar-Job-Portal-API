fb46d9e8518d9561dd85e3afa0b01e9890780d2b
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-07 02:07:30 +0530

    update : client/functions/category
    - created function to read a single category data based on slug

--- client/src/functions/categories.js
+++ client/src/functions/categories.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 //CRUD
-const url = 'http://localhost:8000/api/admin';
+const url = "http://localhost:8000/api/admin";
 
 export const createCategory = async (values) => {
 	return await axios.post(
@@ -9,16 +9,25 @@ export const createCategory = async (values) => {
 		{ values },
 		{
 			headers: {
-				'auth-token': localStorage.getItem('token'),
+				"auth-token": localStorage.getItem("token"),
 			},
-		},
+		}
 	);
 };
 
 export const getAllCategories = async () => {
 	return await axios.get(`${url}/categories`, {
 		headers: {
-			'auth-token': localStorage.getItem('token'),
+			"auth-token": localStorage.getItem("token"),
+		},
+	});
+};
+
+//read a single category
+export const readCategory = async (slug) => {
+	return await axios.get(`${url}/category/${slug}`, {
+		headers: {
+			"auth-token": localStorage.getItem("token"),
 		},
 	});
 };
