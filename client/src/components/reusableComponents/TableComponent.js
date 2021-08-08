b8e41fba09c5efe91ed05a43f8519c2b13b52f48
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-08 12:33:46 +0530

    tableComponent.js

--- client/src/components/reusableComponents/TableComponent.js
+++ client/src/components/reusableComponents/TableComponent.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import { Table, Space } from 'antd';
-import 'antd/dist/antd.css';
+import React from "react";
+import { Table, Space } from "antd";
+import "antd/dist/antd.css";
 const { Column } = Table;
 const TableComponent = ({ data }) => {
+	const { role } = data;
 	return (
 		<Table dataSource={data}>
 			<Column title="First Name" dataIndex="firstName" key="firstName" />
 			<Column title="Last Name" dataIndex="lastName" key="mobileNumber" />
 
-			<Column
-				title="Mobile No."
-				dataIndex="mobileNumber"
-				key="mobileNumber"
-			/>
+			<Column title="Mobile No." dataIndex="mobileNumber" key="mobileNumber" />
 			<Column title="Address" dataIndex="address" key="address" />
+
+			<Column
+				render={(record) => {
+					record.role === "Recruiter" ? (
+						<Column title="verfied" dataIndex="address" key="address" />
+					) : (
+						<Column title="hmm" dataIndex="address" key="address" />
+					);
+				}}
+			/>
+			{}
+
 			{}
 			<Column
 				title="Action"
 				render={(record) => (
 					<Space size="middle">
-						{record.role === 'Recruiter' ? (
+						{record.role === "Recruiter" ? (
 							<a href="/">Verify</a>
 						) : (
 							<a href="/">Veiw</a>
