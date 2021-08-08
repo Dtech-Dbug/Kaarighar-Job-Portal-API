1948694680a57b4567c848e1b2218aec1e803f7e
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-08 21:38:10 +0530

    Created model for jobListing

---
+++ server/Model/jobListings.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const { ObjectId } = Schema;
+
+const jobListingSchema = new Schema(
+	{
+		title: {
+			type: String,
+			trim: true,
+			minLength: ["3", "Too Short"],
+			text: true,
+		},
+		slug: {
+			type: String,
+			trim: true,
+			lowercase: true,
+			index: true,
+			text: true,
+		},
+		parent: {
+			type: ObjectId,
+			ref: "CATEGORIES",
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("JOBS", jobListingSchema);
