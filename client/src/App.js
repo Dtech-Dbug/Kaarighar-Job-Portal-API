2cefb80c3a89893d66f551054e230157ad048057
ashishxcode <ashish.patel0903@gmail.com>
2021-08-10 15:56:56 +0530

    Create custom routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Login from './components/auth/Login';
-import Dashboard from './components/dashboard/Dashboard';
-import Users from './components/users/Users';
-import Home from './components/Home';
-import Category from './components/category/Category';
-import EditCategory from './components/category/editCategory';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Routes from './routing/Routes';
 function App() {
 	return (
 		<div>
 			<Router>
-				<Route path="/" exact component={Dashboard} />
-				
-				<Route path="/login" exact component={Login} />
-				<Route path="/users" exact component={Users} />
-				<Route path="/home" exact component={Home} />
-				<Route path="/category" exact component={Category} />
-				<Route path="/category/:slug" exact component={EditCategory} />
+				<Switch>
+					<Route component={Routes} />
+				</Switch>
 			</Router>
 		</div>
 	);
