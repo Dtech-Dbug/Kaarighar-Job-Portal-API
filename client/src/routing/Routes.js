42b6e34431018c1137c0e411719e95911c46a330
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-10 22:15:18 +0530

    deps installed ,

--- client/src/routing/Routes.js
+++ client/src/routing/Routes.js
@@ -1,10 +1,10 @@
-import { Route, Switch } from 'react-router-dom';
-import PrivateRoute from './PrivateRoute';
-import Dashboard from '../components/dashboard/Dashboard';
-import Users from '../components/users/Users';
-import Home from '../components/Home';
-import Category from '../components/category/Category';
-import EditCategory from '../components/category/EditCategory';
+import { Route, Switch } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import Dashboard from "../components/dashboard/Dashboard";
+import Users from "../components/users/Users";
+import Home from "../components/Home";
+import Category from "../components/category/Category";
+// import EditCategory from "../components/category/EditCategory";
 
 const Routes = (props) => {
 	return (
@@ -13,11 +13,7 @@ const Routes = (props) => {
 			<PrivateRoute exact path="/dashboard" component={Dashboard} />
 			<PrivateRoute exact path="/users" component={Users} />
 			<PrivateRoute exact path="/category" component={Category} />
-			<PrivateRoute
-				exact
-				path="/category/:slug"
-				component={EditCategory}
-			/>
+			{/* <PrivateRoute exact path="/category/:slug" component={EditCategory} /> */}
 		</Switch>
 	);
 };
