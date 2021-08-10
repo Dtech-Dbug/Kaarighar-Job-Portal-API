475c1e3ee18c1e69d60e6c1c18d4f8b5a01b8ed1
ashishxcode <ashish.patel0903@gmail.com>
2021-08-10 16:25:53 +0530

    Create action types and auth action

---
+++ client/src/actions/auth.js
@@ -0,0 +1,53 @@
+import API from '../utils/api';
+import {
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+	USER_LOADED,
+	LOGOUT,
+	AUTH_ERROR,
+} from './types';
+
+// Load User
+export const loadUser = () => async (dispatch) => {
+	try {
+		const res = await API.get('/user');
+
+		dispatch({
+			type: USER_LOADED,
+			payload: res.data,
+		});
+	} catch (err) {
+		dispatch({
+			type: AUTH_ERROR,
+		});
+	}
+};
+
+// Login User
+export const login = (values) => async (dispatch) => {
+	const body = values;
+
+	try {
+		const res = await API.post('/login', body);
+
+		dispatch({
+			type: LOGIN_SUCCESS,
+			payload: res.data,
+		});
+
+		dispatch(loadUser());
+	} catch (err) {
+		const errors = err.response.data.errors;
+
+		if (errors) {
+			console.log('errors');
+		}
+
+		dispatch({
+			type: LOGIN_FAIL,
+		});
+	}
+};
+
+// Logout
+export const logout = () => ({ type: LOGOUT });
