03b6193f775f2a0f6b42aebf89e9e737f7807055
ashishxcode <ashish.patel0903@gmail.com>
2021-08-10 16:25:24 +0530

    Created redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,34 @@
-import './App.css';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { loadUser } from './actions/auth';
+import { LOGOUT } from './actions/types';
 import Routes from './routing/Routes';
+import store from './store';
+import setAuthToken from './utils/setAuthToken';
+
 function App() {
+	useEffect(() => {
+		// check for token in LS
+		if (localStorage.token) {
+			setAuthToken(localStorage.token);
+		}
+		store.dispatch(loadUser());
+
+		// log user out from all tabs if they log out in one tab
+		window.addEventListener('storage', () => {
+			if (!localStorage.token) store.dispatch({ type: LOGOUT });
+		});
+	}, []);
+
 	return (
-		<div>
+		<Provider store={store}>
 			<Router>
 				<Switch>
 					<Route component={Routes} />
 				</Switch>
 			</Router>
-		</div>
+		</Provider>
 	);
 }
 
