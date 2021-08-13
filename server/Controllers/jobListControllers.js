6836b389cd5490ea377437ef99d86148b75a794e
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-13 22:39:23 +0530

    .

--- server/Controllers/jobListControllers.js
+++ server/Controllers/jobListControllers.js
@@ -1,115 +1,112 @@
-const slugify = require('slugify');
-const CATEGORIES = require('../Model/jobCategories');
-const JOBS = require('../Model/jobListings');
-const USER = require('../Model/user');
+const slugify = require("slugify");
+const CATEGORIES = require("../Model/jobCategories");
+const JOBS = require("../Model/jobListings");
+const USER = require("../Model/user");
 
 //createJob , listAllJobs, readJob , udpdateJob , deleteJob
 
 exports.createJob = async (req, res) => {
-	try {
-		const { name, parent, image, noRole, description, requirement } =
-			req.body;
-		const { id } = req.user;
+  try {
+    const { name, parent, image, noRole, description, requirement } = req.body;
+    const { id } = req.user;
 
-		const job = await new JOBS({
-			name,
-			parent,
-			noRole,
-			image,
-			requirement,
-			description,
-			slug: slugify(name),
-			recruiter: id,
-		}).save();
+    const job = await new JOBS({
+      name,
+      parent,
+      noRole,
+      image,
+      requirement,
+      description,
+      slug: slugify(name),
+      recruiter: id,
+    }).save();
 
-		console.log('JOB CREATED--->', job);
-		res.json(job);
-	} catch (err) {
-		console.log('ERROR WHILE CREATING JOB-->', err.message);
-	}
+    console.log("JOB CREATED--->", job);
+    res.json(job);
+  } catch (err) {
+    console.log("ERROR WHILE CREATING JOB-->", err.message);
+  }
 };
 
 exports.listAllJobs = async (req, res) => {
-	try {
-		// get all jobs with last created at
+  try {
+    // get all jobs with last created at
 
-		const jobs = await JOBS.find({})
-			.sort({ createdAt: -1 })
-			.populate('parent', 'title')
-			.populate(
-				'recruiter',
-				'firstName lastName companyName companyRegNo location',
-			)
-			.exec();
+    const jobs = await JOBS.find({})
+      .sort({ createdAt: -1 })
+      .populate("parent", "title")
+      .populate(
+        "recruiter",
+        "firstName lastName companyName companyRegNo location"
+      )
+      .exec();
 
-		console.log('JOBS-->', jobs);
+    console.log("JOBS-->", jobs);
 
-		res.json(jobs);
-	} catch (err) {
-		console.log('ERROR WHIILE LISING JOBS-->', err.message);
-	}
+    res.json(jobs);
+  } catch (err) {
+    console.log("ERROR WHIILE LISING JOBS-->", err.message);
+  }
 };
 
 exports.readJob = async (req, res) => {
-	try {
-		const { slug } = req.params;
-		const job = await JOBS.findOne({ slug }).exec();
-		console.log('JOB-->', job);
-		res.json(job);
-	} catch (err) {
-		console.log('ERROR WHILE READING JOB-->', err.message);
-	}
+  try {
+    const { slug } = req.params;
+    const job = await JOBS.findOne({ slug }).exec();
+    console.log("JOB-->", job);
+    res.json(job);
+  } catch (err) {
+    console.log("ERROR WHILE READING JOB-->", err.message);
+  }
 };
 
 // updateJob
 exports.updateJob = async (req, res) => {
-	try {
-		const { name, parent, image, noRole, description, requirement } =
-			req.body;
-		const { id } = req.user;
+  try {
+    const { name, parent, image, noRole, description, requirement } = req.body;
+    const { id } = req.user;
 
-		const job = await JOBS.findOneAndUpdate(
-			{ slug: req.params.slug },
-			{
-				name,
-				parent,
-				noRole,
-				image,
-				requirement,
-				description,
-				slug: slugify(name),
-				recruiter: id,
-			},
-		).exec();
+    const job = await JOBS.findOneAndUpdate(
+      { slug: req.params.slug },
+      {
+        name,
+        parent,
+        noRole,
+        image,
+        requirement,
+        description,
+        slug: slugify(name),
+        recruiter: id,
+      }
+    ).exec();
 
-		console.log('JOB UPDATED-->', job);
-		res.json(job);
-	} catch (err) {
-		console.log('ERROR WHILE UPDATING JOB-->', err.message);
-	}
+    console.log("JOB UPDATED-->", job);
+    res.json(job);
+  } catch (err) {
+    console.log("ERROR WHILE UPDATING JOB-->", err.message);
+  }
 };
 
 exports.deleteJob = async (req, res) => {
-	try {
-		const { slug } = req.params;
-		const job = await JOBS.findOneAndRemove({ slug }).exec();
-		console.log('JOB DELETED-->', job);
-		res.json(job);
-	} catch (err) {
-		console.log('ERROR WHILE DELETING JOB-->', err.message);
-	}
+  try {
+    const { slug } = req.params;
+    const job = await JOBS.findOneAndRemove({ slug }).exec();
+    console.log("JOB DELETED-->", job);
+    res.json(job);
+  } catch (err) {
+    console.log("ERROR WHILE DELETING JOB-->", err.message);
+  }
 };
 
-exports.readJobsBasedOnCategories = async(req, res) =>{
-try {
-	const category =  req.params.categoryId;
-	console.log(typeof(category))
+exports.readJobsBasedOnCategories = async (req, res) => {
+  try {
+    const category = req.params.categoryId;
+    console.log(typeof category);
 
-	const jobs = await JOBS.find({parent : category}).exec()
+    const jobs = await JOBS.find({ parent: category }).exec();
 
-	res.json(jobs)
-}
-catch(err){
-	console.log('Error message->', err.message)
-}
-}
+    res.json(jobs);
+  } catch (err) {
+    console.log("Error message->", err.message);
+  }
+};
