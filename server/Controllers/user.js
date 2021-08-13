613fd578e84aa4839d3d8144030da66052547ec5
ashishxcode <ashish.patel0903@gmail.com>
2021-08-13 22:54:24 +0530

    Add hashing password feature

--- server/Controllers/user.js
+++ server/Controllers/user.js
@@ -1,228 +1,237 @@
-const UserModel = require("../Model/user");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const UserModel = require('../Model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 exports.getCurrentUser = async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.user.id).select("-password");
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
+	try {
+		const user = await UserModel.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 };
 exports.getUserByID = async (req, res) => {
-  try {
-    console.log("PARAMS-->", req.params.id);
-    const user = await UserModel.findById(req.params.id).select("-password");
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
+	try {
+		console.log('PARAMS-->', req.params.id);
+		const user = await UserModel.findById(req.params.id).select(
+			'-password',
+		);
+		res.json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 };
 
 exports.getAdmin = async (req, res) => {
-  try {
-    const admin = await UserModel.findOne({ role: "Admin" });
-    res.json(admin);
-    console.log(admin);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
+	try {
+		const admin = await UserModel.findOne({ role: 'Admin' });
+		res.json(admin);
+		console.log(admin);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 };
 
 exports.registerUser = async (req, res) => {
-  console.log("REQ BODY =>", req.body);
+	console.log('REQ BODY =>', req.body);
 
-  const {
-    firstName,
-    lastName,
-    email,
-    mobileNumber,
-    aadharCard,
-    panCard,
-    address,
-    pinCode,
-    city,
-    role,
-    companyName,
-    companyRegNo,
-    location,
-  } = req.body;
+	const {
+		firstName,
+		lastName,
+		email,
+		mobileNumber,
+		aadharCard,
+		panCard,
+		address,
+		pinCode,
+		city,
+		role,
+		companyName,
+		companyRegNo,
+		location,
+	} = req.body;
 
-  var { password } = req.body;
+	var { password } = req.body;
 
-  //@desc: Creating user using register route
-  //@input: Registet from data
-  //@output: JWT token for user (with expiry)
+	//@desc: Creating user using register route
+	//@input: Registet from data
+	//@output: JWT token for user (with expiry)
 
-  try {
-    let user = await UserModel.findOne({ mobileNumber });
+	try {
+		let user = await UserModel.findOne({ mobileNumber });
 
-    console.log("USER--->", user);
+		console.log('USER--->', user);
 
-    if (user) {
-      return res.json({ ok: true });
-    }
+		if (user) {
+			return res.json({ ok: true });
+		}
 
-    //generation
-    const salt = await bcrypt.genSalt(10);
+		//generation
+		const salt = await bcrypt.genSalt(10);
 
-    //password
-    hashedPassword = await bcrypt.hash(password, salt);
+		//password
+		hashedPassword = await bcrypt.hash(password, salt);
 
-    // Creating user object and save
+		// Creating user object and save
 
-    if (req.body.role === "Job Seeker") {
-      user = await new UserModel({
-        firstName,
-        lastName,
-        email,
-        password: hashedPassword,
-        mobileNumber,
-        aadharCard,
-        panCard,
-        address,
-        pinCode,
-        city,
-        role,
-      }).save();
-    }
+		if (req.body.role === 'Job Seeker') {
+			user = await new UserModel({
+				firstName,
+				lastName,
+				email,
+				password: hashedPassword,
+				mobileNumber,
+				aadharCard,
+				panCard,
+				address,
+				pinCode,
+				city,
+				role,
+			}).save();
+		}
 
-    if (req.body.role === "Recruiter") {
-      user = await new UserModel({
-        firstName,
-        lastName,
-        email,
-        password: hashedPassword,
-        mobileNumber,
-        address,
-        pinCode,
-        city,
-        role,
-        companyRegNo,
-        companyName,
-        location: {
-          latitude: location.latitude,
-          longitude: location.longitude,
-        },
-      }).save();
-    }
+		if (req.body.role === 'Recruiter') {
+			user = await new UserModel({
+				firstName,
+				lastName,
+				email,
+				password: hashedPassword,
+				mobileNumber,
+				address,
+				pinCode,
+				city,
+				role,
+				companyRegNo,
+				companyName,
+				location: {
+					latitude: location.latitude,
+					longitude: location.longitude,
+				},
+			}).save();
+		}
 
-    await console.log("User saved->", user);
+		await console.log('User saved->', user);
 
-    const payload = {
-      user: {
-        id: user.id,
-        role: user.role,
-      },
-    };
+		const payload = {
+			user: {
+				id: user.id,
+				role: user.role,
+			},
+		};
 
-    // Creating the JWT token
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: "5 days" },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
-  } catch (err) {
-    console.error(err.message);
+		// Creating the JWT token
+		jwt.sign(
+			payload,
+			process.env.JWT_SECRET,
+			{ expiresIn: '5 days' },
+			(err, token) => {
+				if (err) throw err;
+				res.json({ token });
+			},
+		);
+	} catch (err) {
+		console.error(err.message);
 
-    console.log("ERROR WHILE RESGISTERING-->", err);
-    res.status(500).send("Server error");
-  }
+		console.log('ERROR WHILE RESGISTERING-->', err);
+		res.status(500).send('Server error');
+	}
 };
 
 exports.loginUser = async (req, res) => {
-  try {
-    //get the logIn details from frontend
-    const { mobileNumber, password } = req.body;
+	try {
+		//get the logIn details from frontend
+		const { mobileNumber, password } = req.body;
 
-    //query the databse using the mobileNo to find the user
-    let user = await UserModel.findOne({ mobileNumber });
+		//query the databse using the mobileNo to find the user
+		let user = await UserModel.findOne({ mobileNumber });
 
-    // checking if user exists or not
-    if (!user) {
-      return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
-    }
+		// checking if user exists or not
+		if (!user) {
+			return res
+				.status(400)
+				.json({ errors: [{ msg: 'Invalid Credentials' }] });
+		}
 
-    //compare the req.body.password with the password in the database
-    const isMatch = await bcrypt.compare(password, user.password);
+		//compare the req.body.password with the password in the database
+		const isMatch = await bcrypt.compare(password, user.password);
 
-    //if password not matched then return error
-    if (!isMatch) {
-      return res.status(400).json("Mobile No. or Password is incorrect");
-    }
+		//if password not matched then return error
+		if (!isMatch) {
+			return res.status(400).json('Mobile No. or Password is incorrect');
+		}
 
-    //if password matched then create the token
+		//if password matched then create the token
 
-    // passing payload for the JWT
-    const payload = {
-      user: {
-        id: user.id,
-        role: user.role,
-      },
-    };
+		// passing payload for the JWT
+		const payload = {
+			user: {
+				id: user.id,
+				role: user.role,
+			},
+		};
 
-    // Creating the JWT token
-    const token = jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: "5 days" },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token, login: true });
-      }
-    );
-  } catch (err) {
-    console.error(err.message);
-    console.log("ERROR WHILE LOGGING IN-->", err);
-    res.status(500).send("Server error");
-  }
+		// Creating the JWT token
+		const token = jwt.sign(
+			payload,
+			process.env.JWT_SECRET,
+			{ expiresIn: '5 days' },
+			(err, token) => {
+				if (err) throw err;
+				res.json({ token, login: true });
+			},
+		);
+	} catch (err) {
+		console.error(err.message);
+		console.log('ERROR WHILE LOGGING IN-->', err);
+		res.status(500).send('Server error');
+	}
 };
 
 // getAlluser except admin role
 exports.getUsers = async (req, res) => {
-  const allUsers = await UserModel.find().exec();
-  const users = allUsers.filter((user) => user.role !== "Admin");
-  res.json(users);
+	const allUsers = await UserModel.find().exec();
+	const users = allUsers.filter((user) => user.role !== 'Admin');
+	res.json(users);
 };
 
 //controller function to verify users by admin
 exports.verifyUsers = async (req, res) => {
-  try {
-    //we send userId and boolean value from frontEnd
-    //userId of user(recruiter) we want to verify
-    const { userId, verifiedState } = req.body;
-    console.log(req.body);
-    // find user by id and update verified state
-    const findUserAndUpdate = await UserModel.findByIdAndUpdate(
-      { _id: userId },
-      { verified: verifiedState },
-      { new: true }
-    ).exec();
-    console.log("Recruiter Verified");
-    res.json(findUserAndUpdate);
-  } catch (err) {
-    console.log("ERROR WHILE UDATING", err.message);
-  }
+	try {
+		//we send userId and boolean value from frontEnd
+		//userId of user(recruiter) we want to verify
+		const { userId, verifiedState } = req.body;
+		console.log(req.body);
+		// find user by id and update verified state
+		const findUserAndUpdate = await UserModel.findByIdAndUpdate(
+			{ _id: userId },
+			{ verified: verifiedState },
+			{ new: true },
+		).exec();
+		console.log('Recruiter Verified');
+		res.json(findUserAndUpdate);
+	} catch (err) {
+		console.log('ERROR WHILE UDATING', err.message);
+	}
 };
 
 exports.resetPassword = async (req, res) => {
-  const id = req.params.userId;
-  console.log("REQ BODY FOR PW-->", req.body);
-  const password = req.body.password;
+	const id = req.params.userId;
+	console.log('REQ BODY FOR PW-->', req.body);
+	const password = req.body.password;
+	//generation
+	const salt = await bcrypt.genSalt(10);
 
-  const updatePassword = await UserModel.findByIdAndUpdate(
-    { _id: id },
-    { password },
-    { new: true }
-  ).exec();
+	//password
+	hashedPassword = await bcrypt.hash(password, salt);
 
-  res.json(updatePassword);
+	const updatePassword = await UserModel.findByIdAndUpdate(
+		{ _id: id },
+		{ password: hashedPassword },
+		{ new: true },
+	).exec();
+
+	res.json(updatePassword);
 };
