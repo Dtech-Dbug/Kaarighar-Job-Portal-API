4491422ee31964664e217603c219aea03becd5e5
ashishxcode <ashish.patel0903@gmail.com>
2021-08-13 23:25:28 +0530

    Removed commented code

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -44,55 +44,6 @@ const Login = ({ login, isAuthenticated }) => {
 						Sign in to your account
 					</h2>
 				</div>
-				{/* <form className="mt-8 space-y-6" action="#" method="POST">
-					<input type="hidden" name="remember" defaultValue="true" />
-					<div className="rounded-md shadow-sm text-left -space-y-px">
-						<div className="mb-4 mr-1">
-							<label
-								className="block text-grey-darker text-sm font-bold mb-2"
-								htmlFor="mobile-number">
-								Mobile No.{' '}
-								<span className="text-red-900">*</span>
-							</label>
-							<input
-								className="appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-								required
-								name="mobileNumber"
-								type="tel"
-								placeholder="Your mobile no."
-								value={formValue.mobileNumber || ''}
-								onChange={handleChange}
-							/>
-						</div>
-
-						<div className="mb-4">
-							<label
-								className="block text-grey-darker text-sm font-bold mb-2"
-								htmlFor="password">
-								Password <span className="text-red-900">*</span>
-							</label>
-							<input
-								className="appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-								name="password"
-								type="password"
-								required
-								placeholder="Password"
-								value={formValue.password || ''}
-								onChange={handleChange}
-							/>
-						</div>
-					</div>
-
-					<div>
-						<button
-							type="submit"
-							className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-							onClick={handleClick}>
-							<span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
-							Sign in
-						</button>
-					</div>
-				</form> */}
 				<Form
 					initialValues={{
 						remember: true,
