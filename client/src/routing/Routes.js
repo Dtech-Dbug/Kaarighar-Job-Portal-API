09d6ebac8bb21fccbda8f9acadef1d8a2ee3e46f
ashishxcode <ashish.patel0903@gmail.com>
2021-08-14 01:24:12 +0530

    Added jobs route

--- client/src/routing/Routes.js
+++ client/src/routing/Routes.js
@@ -1,10 +1,11 @@
-import { Route, Switch } from "react-router-dom";
-import PrivateRoute from "./PrivateRoute";
-import Dashboard from "../components/dashboard/Dashboard";
-import Users from "../components/users/Users";
-import Home from "../components/Home";
-import Category from "../components/category/Category";
-import ViewProfile from "../components/users/ViewProfile";
+import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+import Dashboard from '../components/dashboard/Dashboard';
+import Users from '../components/users/Users';
+import Home from '../components/Home';
+import Category from '../components/category/Category';
+import ViewProfile from '../components/users/ViewProfile';
+import Jobs from '../components/job/jobs';
 // import EditCategory from "../components/category/EditCategory";
 
 const Routes = (props) => {
@@ -15,6 +16,7 @@ const Routes = (props) => {
 			<PrivateRoute exact path="/users" component={Users} />
 			<PrivateRoute exact path="/user/:id" component={ViewProfile} />
 			<PrivateRoute exact path="/category" component={Category} />
+			<PrivateRoute exact path="/jobs" component={Jobs} />
 			{/* <PrivateRoute exact path="/category/:slug" component={EditCategory} /> */}
 		</Switch>
 	);
