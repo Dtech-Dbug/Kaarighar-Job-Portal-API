5d0e5108f383c13ea353d27f8dd46ff509334d4a
ashishxcode <ashish.patel0903@gmail.com>
2021-08-14 19:12:09 +0530

    Update : User Model

--- server/Model/user.js
+++ server/Model/user.js
@@ -1,74 +1,135 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
-  {
-    firstName: {
-      type: String,
-      required: "Name is Required",
-    },
-    lastName: {
-      type: String,
-      required: "Name is Required",
-    },
-    mobileNumber: {
-      type: String,
-      required: "Name is Required",
-    },
-    password: {
-      type: String,
-      required: "Name is Required",
-    },
+	{
+		firstName: {
+			type: String,
+			required: 'First Name is Required',
+		},
 
-    email: String,
+		lastName: {
+			type: String,
+			required: 'Last Name is Required',
+		},
 
-    aadharCard: Number,
+		mobileNumber: {
+			type: String,
+			required: 'Mobile Number is Required',
+		},
 
-    panCard: String,
+		password: {
+			type: String,
+			required: 'Password is Required',
+		},
 
-    companyName: String,
+		email: String,
 
-    companyRegNo: String,
+		aadharCard: {
+			aadharNumber: Number,
+			aadharImage: String,
+		},
 
-    address: String,
+		panCard: {
+			panNumber: String,
+			panImage: String,
+		},
 
-    pinCode: Number,
+		company: {
+			companyName: String,
+			companyRegNo: String,
+			companyAddress: String,
+			companyContact: String,
+		},
 
-    city: String,
+		address: {
+			addressLine1: String,
+			addressLine2: String,
+			city: String,
+			state: String,
+			country: String,
+			zipCode: String,
+			latitude: Number,
+			longitude: Number,
+		},
 
-    location: {
-      latitude: Number,
-      longitude: Number,
-    },
+		profile: {
+			skills: {
+				type: Array,
+			},
 
-    verified: { type: Boolean, default: false },
+			about: {
+				type: String,
+			},
+			experience: [
+				{
+					title: {
+						type: String,
+						required: true,
+					},
+					company: {
+						type: String,
+						required: true,
+					},
+					location: {
+						type: String,
+					},
+					from: {
+						type: Date,
+						required: true,
+					},
+					to: {
+						type: Date,
+					},
+					current: {
+						type: Boolean,
+						default: false,
+					},
+					description: {
+						type: String,
+					},
+				},
+			],
+			education: [
+				{
+					school: {
+						type: String,
+						required: true,
+					},
+					degree: {
+						type: String,
+						required: true,
+					},
+					fieldofstudy: {
+						type: String,
+						required: true,
+					},
+					from: {
+						type: Date,
+						required: true,
+					},
+					to: {
+						type: Date,
+					},
+					current: {
+						type: Boolean,
+						default: false,
+					},
+					description: {
+						type: String,
+					},
+				},
+			],
+		},
 
-    role: {
-      type: String,
-      enum: ["Admin", "Job Seeker", "Recruiter"],
-    },
+		verified: { type: Boolean, default: false },
 
-    userProfile: {
-      skills: {
-        type: String,
-      },
-
-      about: {
-        type: String,
-      },
-
-      experience: {
-        type: String,
-
-        work: {
-          companyName: String,
-          role: String,
-          duration: Number,
-        },
-      },
-    },
-  },
-  { timestamps: true }
+		role: {
+			type: String,
+			enum: ['Admin', 'Job Seeker', 'Recruiter'],
+		},
+	},
+	{ timestamps: true },
 );
 
-module.exports = mongoose.model("USER", userSchema);
+module.exports = mongoose.model('USER', userSchema);
