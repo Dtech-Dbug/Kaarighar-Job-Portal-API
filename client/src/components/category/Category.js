92dd821777f2380d0f16c215b88ab2ac54226d53
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-15 21:25:09 +0530

    editCategory : styled

--- client/src/components/category/Category.js
+++ client/src/components/category/Category.js
@@ -1,74 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import CategoryForm from './CategoryForm';
-import CategoryList from './CategoryList';
+import React, { useState, useEffect } from "react";
+import CategoryForm from "./CategoryForm";
+import CategoryList from "./CategoryList";
 import {
-	createCategory,
-	getAllCategories,
-	deleteCategory,
-} from '../../functions/categories';
+  createCategory,
+  getAllCategories,
+  deleteCategory,
+  editCategory,
+} from "../../functions/categories";
 
-import { RiLayout2Line } from 'react-icons/ri';
+import { RiLayout2Line } from "react-icons/ri";
 
-const Category = () => {
-	const [formData, setFormData] = useState('');
-	const [categoryList, setCategoryList] = useState([]);
+const Category = ({ match, history }) => {
+  const [formData, setFormData] = useState("");
+  const [categoryList, setCategoryList] = useState([]);
 
-	useEffect(() => {
-		fetchCategories();
-	}, []);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
-	const fetchCategories = () =>
-		getAllCategories()
-			.then((categories) => {
-				setCategoryList(categories.data);
-			})
-			.catch((err) => alert(err.message));
+  const fetchCategories = () =>
+    getAllCategories()
+      .then((categories) => {
+        setCategoryList(categories.data);
+      })
+      .catch((err) => alert(err.message));
 
-	const handleCategoryChange = (e) => {
-		setFormData(e.target.value);
-	};
+  const handleCategoryChange = (e) => {
+    setFormData(e.target.value);
+  };
 
-	const handleCategoryFormSubmit = (e, values) => {
-		e.preventDefault();
+  const handleCategoryFormSubmit = (e, values) => {
+    e.preventDefault();
 
-		//making req to BE to create category
-		createCategory(formData)
-			.then((res) => {
-				// alert("CATEGORY CREATED");
-				window.location.reload();
-			})
-			.catch((err) => console.log(err.message));
-	};
+    //making req to BE to create category
+    createCategory(formData)
+      .then((res) => {
+        // alert("CATEGORY CREATED");
+        window.location.reload();
+      })
+      .catch((err) => console.log(err.message));
+  };
 
-	const handleCategoryDelete = (slug) => {
-		let confirm = window.confirm('Are you sure , you want to delete?');
-		if (confirm) {
-			deleteCategory(slug)
-				.then((res) => {
-					fetchCategories();
-				})
-				.catch((err) =>
-					alert('Error while deleting in frontend', err.message),
-				);
-		}
-	};
+  //   const handleCategoryFormEditSubmit = (e, values) => {
+  //     e.preventDefault();
+  //     console.log(formData);
 
-	return (
-		<>
-			<h1 className="flex items-center font-bold text-lg py-1.5">
-				<RiLayout2Line />
-				&nbsp;Category
-			</h1>
-			<CategoryForm
-				handleCategoryChange={handleCategoryChange}
-				handleCategoryFormSubmit={handleCategoryFormSubmit}
-			/>
-			<CategoryList
-				categoryList={categoryList}
-				handleCategoryDelete={handleCategoryDelete}
-			/>
-		</>
-	);
+  //     //making req to BE to create category
+  //     editCategory(match.params.slug, formData)
+  //       .then((res) => {
+  //         // alert("CATEGORY CREATED");
+  //         history.pushState("/category");
+  //       })
+  //       .catch((err) => console.log(err.message));
+  //   };
+
+  const handleCategoryDelete = (slug) => {
+    let confirm = window.confirm("Are you sure , you want to delete?");
+    if (confirm) {
+      deleteCategory(slug)
+        .then((res) => {
+          fetchCategories();
+        })
+        .catch((err) => alert("Error while deleting in frontend", err.message));
+    }
+  };
+
+  return (
+    <>
+      <h1 className="flex items-center font-bold text-lg py-1.5">
+        <RiLayout2Line />
+        &nbsp;Category
+      </h1>
+      <CategoryForm
+        handleCategoryChange={handleCategoryChange}
+        handleCategoryFormSubmit={handleCategoryFormSubmit}
+        // handleCategoryFormEditSubmit={handleCategoryFormEditSubmit}
+      />
+      <CategoryList
+        categoryList={categoryList}
+        handleCategoryDelete={handleCategoryDelete}
+      />
+    </>
+  );
 };
 
 export default Category;
--- client/src/components/category/editCategory.js
+++ client/src/components/category/editCategory.js
@@ -1,55 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import { readCategory, editCategory } from '../../functions/categories';
-import CategoryForm from './CategoryForm';
+import React, { useState, useEffect } from "react";
+import { readCategory, editCategory } from "../../functions/categories";
+import CategoryForm from "./CategoryForm";
+import { Card, Form, Button, Input, Upload, message } from "antd";
+import "antd/dist/antd.css";
 
 //function to fetch data of a sinle category
 
 const EditCategory = ({ match, history }) => {
-	const [category, setCategory] = useState();
-	const [formValue, setFormValue] = useState('');
+  const [category, setCategory] = useState();
+  const [formValue, setFormValue] = useState("");
 
-	useEffect(() => {
-		fetchData();
-	}, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-	// const { title } = category;
+  // const { title } = category;
 
-	const fetchData = () =>
-		readCategory(match.params.slug).then((res) => {
-			console.log('Categoty:', res);
-			setCategory(res.data);
-			setFormValue(res.data.title);
-		});
+  const fetchData = () =>
+    readCategory(match.params.slug).then((res) => {
+      console.log("Categoty:", res);
+      console.log("RES -->", res.data.title);
 
-	const handleEditFormSubmit = (e) => {
-		e.preventDefault();
-		console.log('Form value', formValue);
-		editCategory(match.params.slug, formValue)
-			.then((res) => history.push('/category'))
-			.catch((err) => console.log(err.message));
-	};
+      setCategory(res.data);
+    });
+  const handleCategoryChange = (e) => {
+    setFormValue(e.target.value);
+  };
+  const handleEditFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form value", formValue);
+    editCategory(match.params.slug, formValue)
+      .then((res) => history.push("/category"))
+      .catch((err) => console.log(err.message));
+  };
 
-	const editCategoryForm = () => (
-		<form>
-			<input
-				type="text"
-				// value={category.title}
-				value={formValue}
-				onChange={(e) => setFormValue(e.target.value)}
-			/>
-			<button onClick={handleEditFormSubmit}>Submit</button>
-		</form>
-	);
+  const editCategoryForm = () => (
+    <Card className="w-full">
+      <Form name="Add Category" layout="vertical">
+        <Form.Item
+          label="Category Name"
+          name="category-name"
+          rules={[
+            {
+              required: true,
+              message: "Please input category name!",
+            },
+          ]}
+        >
+          <Input
+            // placeholder="Enter Category Name."
+            className="p-2"
+            value={match.params.slug}
+            name={formValue}
+            onChange={handleCategoryChange}
+          />
+        </Form.Item>
+        <Button
+          onClick={handleEditFormSubmit}
+          type="primary"
+          size="large"
+          className=" my-4"
+        >
+          Add Category
+        </Button>
+      </Form>
+    </Card>
+  );
 
-	console.log('Match', match);
-	return (
-		<>
-			{/* <h1>Edit category : {match.params.slug} </h1> */}
-			<br />
-			<div>{editCategoryForm()}</div>
-			{/* <CategoryForm /> */}
-		</>
-	);
+  console.log("Match", match);
+  return (
+    <>
+      {/* <h1>Edit category : {match.params.slug} </h1> */}
+      <br />
+      <div>{editCategoryForm()}</div>
+      {/* <CategoryForm /> */}
+    </>
+  );
 };
 
 export default EditCategory;
