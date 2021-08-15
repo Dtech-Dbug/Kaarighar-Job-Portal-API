e020b5bd2e6002c2c7558f879c0272a4982c1b4f
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-15 21:25:24 +0530

    categoryForm

--- client/src/components/category/CategoryForm.js
+++ client/src/components/category/CategoryForm.js
@@ -1,74 +1,78 @@
-import React from 'react';
+import React from "react";
 
-import { Card, Form, Button, Input, Upload, message } from 'antd';
-import { InboxOutlined } from '@ant-design/icons';
-import 'antd/dist/antd.css';
+import { Card, Form, Button, Input, Upload, message } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
+import "antd/dist/antd.css";
 const { Dragger } = Upload;
 
-const CategoryForm = ({ handleCategoryChange, handleCategoryFormSubmit }) => {
-	const props = {
-		name: 'file',
-		multiple: true,
-		action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-		onChange(info) {
-			const { status } = info.file;
-			if (status !== 'uploading') {
-				console.log(info.file, info.fileList);
-			}
-			if (status === 'done') {
-				message.success(
-					`${info.file.name} file uploaded successfully.`,
-				);
-			} else if (status === 'error') {
-				message.error(`${info.file.name} file upload failed.`);
-			}
-		},
-		onDrop(e) {
-			console.log('Dropped files', e.dataTransfer.files);
-		},
-	};
+const CategoryForm = ({
+  handleCategoryChange,
+  handleCategoryFormSubmit,
+  handleCategoryFormEditSubmit,
+}) => {
+  const props = {
+    name: "file",
+    multiple: true,
+    action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (status === "done") {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    onDrop(e) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+  };
 
-	return (
-		<Card className="w-full">
-			<Form name="Add Category" layout="vertical">
-				<Form.Item
-					label="Category Name"
-					name="category-name"
-					rules={[
-						{
-							required: true,
-							message: 'Please input category name!',
-						},
-					]}>
-					<Input
-						placeholder="Enter Category Name."
-						className="p-2"
-						onChange={handleCategoryChange}
-					/>
-				</Form.Item>
-				<Dragger {...props}>
-					<p className="ant-upload-drag-icon">
-						<InboxOutlined />
-					</p>
-					<p className="ant-upload-text">
-						Click or drag file to this area to upload
-					</p>
-					<p className="ant-upload-hint px-4">
-						Support for a single or bulk upload. Strictly prohibit
-						from uploading company data or other band files
-					</p>
-				</Dragger>
+  return (
+    <Card className="w-full">
+      <Form name="Add Category" layout="vertical">
+        <Form.Item
+          label="Category Name"
+          name="category-name"
+          rules={[
+            {
+              required: true,
+              message: "Please input category name!",
+            },
+          ]}
+        >
+          <Input
+            placeholder="Enter Category Name."
+            className="p-2"
+            onChange={handleCategoryChange}
+          />
+        </Form.Item>
+        <Dragger {...props}>
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">
+            Click or drag file to this area to upload
+          </p>
+          <p className="ant-upload-hint px-4">
+            Support for a single or bulk upload. Strictly prohibit from
+            uploading company data or other band files
+          </p>
+        </Dragger>
 
-				<Button
-					onClick={handleCategoryFormSubmit}
-					type="primary"
-					size="large"
-					className=" my-4">
-					Add Category
-				</Button>
-			</Form>
-		</Card>
-	);
+        <Button
+          onClick={handleCategoryFormSubmit}
+          type="primary"
+          size="large"
+          className=" my-4"
+        >
+          Add Category
+        </Button>
+      </Form>
+    </Card>
+  );
 };
 
 export default CategoryForm;
