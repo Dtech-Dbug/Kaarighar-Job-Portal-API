48c9babfc3beeed05cf6ba39f52e540e76130ae1
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-15 17:17:07 +0530

    update & fix : client/Routes
     fixed the typo for importing the jobs component

--- client/src/routing/Routes.js
+++ client/src/routing/Routes.js
@@ -1,27 +1,27 @@
-import { Route, Switch } from 'react-router-dom';
-import PrivateRoute from './PrivateRoute';
-import Dashboard from '../components/dashboard/Dashboard';
-import Users from '../components/users/Users';
-import Home from '../components/Home';
-import Category from '../components/category/Category';
-import ViewProfile from '../components/users/ViewProfile';
-import Jobs from '../components/job/Jobs';
-import ViewJob from '../components/job/ViewJob';
+import { Route, Switch } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import Dashboard from "../components/dashboard/Dashboard";
+import Users from "../components/users/Users";
+import Home from "../components/Home";
+import Category from "../components/category/Category";
+import ViewProfile from "../components/users/ViewProfile";
+import Jobs from "../components/job/jobs";
+import ViewJob from "../components/job/ViewJob";
 // import EditCategory from "../components/category/EditCategory";
 
 const Routes = (props) => {
-	return (
-		<Switch>
-			<Route path="/" exact component={Home} />
-			<PrivateRoute exact path="/dashboard" component={Dashboard} />
-			<PrivateRoute exact path="/users" component={Users} />
-			<PrivateRoute exact path="/user/:id" component={ViewProfile} />
-			<PrivateRoute exact path="/category" component={Category} />
-			<PrivateRoute exact path="/jobs" component={Jobs} />
-			<PrivateRoute exact path="/job/:id" component={ViewJob} />
-			{/* <PrivateRoute exact path="/category/:slug" component={EditCategory} /> */}
-		</Switch>
-	);
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <PrivateRoute exact path="/dashboard" component={Dashboard} />
+      <PrivateRoute exact path="/users" component={Users} />
+      <PrivateRoute exact path="/user/:id" component={ViewProfile} />
+      <PrivateRoute exact path="/category" component={Category} />
+      <PrivateRoute exact path="/jobs" component={Jobs} />
+      <PrivateRoute exact path="/job/:id" component={ViewJob} />
+      {/* <PrivateRoute exact path="/category/:slug" component={EditCategory} /> */}
+    </Switch>
+  );
 };
 
 export default Routes;
