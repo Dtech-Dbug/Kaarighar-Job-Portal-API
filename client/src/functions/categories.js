37c67c10196184b686a3526840397f94e56c380e
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-18 12:29:28 +0530

    update and fix : client/fubction/category
    - fixed the request URl

--- client/src/functions/categories.js
+++ client/src/functions/categories.js
@@ -1,53 +1,53 @@
 import axios from "axios";
 
 //CRUD
-const url = "/api/admin";
+const url = "http://localhost:8000/api/admin";
 
 export const createCategory = async (values) => {
-	return await axios.post(
-		`${url}/category`,
-		{ values },
-		{
-			headers: {
-				"auth-token": localStorage.getItem("token"),
-			},
-		}
-	);
+  return await axios.post(
+    `${url}/category`,
+    { values },
+    {
+      headers: {
+        "auth-token": localStorage.getItem("token"),
+      },
+    }
+  );
 };
 
 export const getAllCategories = async () => {
-	return await axios.get(`${url}/categories`, {
-		headers: {
-			"auth-token": localStorage.getItem("token"),
-		},
-	});
+  return await axios.get(`${url}/categories`, {
+    headers: {
+      "auth-token": localStorage.getItem("token"),
+    },
+  });
 };
 
 //read a single category
 export const readCategory = async (slug) => {
-	return await axios.get(`${url}/category/${slug}`, {
-		headers: {
-			"auth-token": localStorage.getItem("token"),
-		},
-	});
+  return await axios.get(`${url}/category/${slug}`, {
+    headers: {
+      "auth-token": localStorage.getItem("token"),
+    },
+  });
 };
 
 export const editCategory = async (slug, values) => {
-	return await axios.put(
-		`${url}/category/${slug}`,
-		{ values },
-		{
-			headers: {
-				"auth-token": localStorage.getItem("token"),
-			},
-		}
-	);
+  return await axios.put(
+    `${url}/category/${slug}`,
+    { values },
+    {
+      headers: {
+        "auth-token": localStorage.getItem("token"),
+      },
+    }
+  );
 };
 
 export const deleteCategory = async (slug, values) => {
-	return await axios.delete(`${url}/category/${slug}`, {
-		headers: {
-			"auth-token": localStorage.getItem("token"),
-		},
-	});
+  return await axios.delete(`${url}/category/${slug}`, {
+    headers: {
+      "auth-token": localStorage.getItem("token"),
+    },
+  });
 };
