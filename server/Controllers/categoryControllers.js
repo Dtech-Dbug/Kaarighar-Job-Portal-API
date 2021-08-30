25a4e7d99083c73e147ea28f9536cb6d8f27c4b1
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-30 12:39:44 +0530

    update : categoryControllers
    - corrected the value receiving

--- server/Controllers/categoryControllers.js
+++ server/Controllers/categoryControllers.js
@@ -4,80 +4,82 @@ const CATEGORIES = require("../Model/jobCategories");
 //controller functions for category CRUD
 
 exports.createCategory = async (req, res) => {
-	try {
-		console.log("REQ FOR CREATING CATEGORY", req.body);
-		console.log("TITLe->", req.body.values);
+  try {
+    console.log("REQ FOR CREATING CATEGORY", req.body);
+    console.log("TITLe->", req.body.values);
 
-		const { values } = req.body;
+    const { title } = req.body.values;
+    const { url } = req.body.values;
 
-		const category = await new CATEGORIES({
-			title: values,
-			slug: slugify(values),
-		}).save();
-		console.log("Category created w/ title :", category);
-		res.json(category);
-	} catch (err) {
-		console.log("ERROR WHILE CREATING CATEGORY", err.message);
-		res.status(400).send("OOPS! Something went wrong.Check the console.");
-	}
+    const category = await new CATEGORIES({
+      title: title,
+      slug: slugify(title),
+      images: url,
+    }).save();
+    console.log("Category created w/ title :", category);
+    res.json(category);
+  } catch (err) {
+    console.log("ERROR WHILE CREATING CATEGORY", err.message);
+    res.status(400).send("OOPS! Something went wrong.Check the console.");
+  }
 };
 
 exports.listAllCategories = async (req, res) => {
-	try {
-		const allCategories = await CATEGORIES.find({})
-			.sort({ createdAt: -1 })
-			.exec();
+  try {
+    const allCategories = await CATEGORIES.find({})
+      .sort({ createdAt: -1 })
+      .exec();
 
-		res.json(allCategories);
-	} catch (err) {
-		console.log(err);
-		res.send("OOPS SOMETHINF WENT WRONG. Check the console.");
-	}
+    res.json(allCategories);
+  } catch (err) {
+    console.log(err);
+    res.send("OOPS SOMETHINF WENT WRONG. Check the console.");
+  }
 };
 
 exports.readCategory = async (req, res) => {
-	try {
-		const category = await CATEGORIES.findOne({ slug: req.params.slug }).exec();
+  try {
+    const category = await CATEGORIES.findOne({ slug: req.params.slug }).exec();
 
-		res.json(category);
-	} catch (err) {
-		console.log("ERROR WHILE LISTING A SINGLE CATEGORY-->", err);
-		res.send("OOPS! Somehing went wrong while reading a single categiry");
-	}
+    res.json(category);
+  } catch (err) {
+    console.log("ERROR WHILE LISTING A SINGLE CATEGORY-->", err);
+    res.send("OOPS! Somehing went wrong while reading a single categiry");
+  }
 };
 
 exports.removeCategory = async (req, res) => {
-	try {
-		const deletedCategory = await CATEGORIES.findOneAndDelete({
-			slug: req.params.slug,
-		}).exec();
-		res.json(deletedCategory);
-	} catch (err) {
-		console.log("ERROR WHILE deleting A SINGLE CATEGORY-->", err);
-		res.send("OOPS! Somehing went wrong while deleting a single categiry");
-	}
+  try {
+    const deletedCategory = await CATEGORIES.findOneAndDelete({
+      slug: req.params.slug,
+    }).exec();
+    res.json(deletedCategory);
+  } catch (err) {
+    console.log("ERROR WHILE deleting A SINGLE CATEGORY-->", err);
+    res.send("OOPS! Somehing went wrong while deleting a single categiry");
+  }
 };
 
 exports.updateCategory = async (req, res) => {
-	try {
-		console.log("EDIT CAT VALUE->", req.body.values);
-		const { values } = req.body;
+  try {
+    console.log("EDIT CAT VALUE->", req.body.values);
+    const { values } = req.body;
 
-		const updatedcategory = await CATEGORIES.findOneAndUpdate(
-			{
-				slug: req.params.slug,
-			},
-			{
-				title: values,
-				slug: slugify(values),
-			},
-			{ new: true }
-		);
+    const updatedcategory = await CATEGORIES.findOneAndUpdate(
+      {
+        slug: req.params.slug,
+      },
+      {
+        title: values,
+        slug: slugify(values),
+      },
+      { new: true }
+    );
 
-		console.log("UPDATED-->", updatedcategory);
-		res.json(updatedcategory);
-	} catch (err) {
-		console.log("ERROR WHILE updating A SINGLE CATEGORY-->", err);
-		res.send("OOPS! Somehing went wrong while updating a single categiry");
-	}
+    console.log("UPDATED-->", updatedcategory);
+    res.json(updatedcategory);
+  } catch (err) {
+    console.log("ERROR WHILE updating A SINGLE CATEGORY-->", err);
+    res.send("OOPS! Somehing went wrong while updating a single categiry");
+  }
 };
