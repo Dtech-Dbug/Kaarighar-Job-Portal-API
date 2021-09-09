374e15722aa5845a7db53cb7907073650f9194a8
ashishxcode <ashish.patel0903@gmail.com>
2021-09-09 15:19:52 +0530

    Removed Server Connection Warnings

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ const app = express();
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    userCreateIndex: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
     useFindAndModify: false,
   })
   .then(() => console.log("DB Conected"))
@@ -34,5 +35,4 @@ readdirSync("./Routes").map((route) =>
 
 app.listen(process.env.PORT, () => {
   console.log(`App Running on ${process.env.PORT}`);
-  console.log(`MONGO URI ON ${process.env.MONGO_URI}`);
 });
