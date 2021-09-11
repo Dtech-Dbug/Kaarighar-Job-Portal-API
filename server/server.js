9bf29a57bdcbcb35621c1b18b44d241babc51be3
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-09-11 21:14:15 +0530

    test : server.js multer congif

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 //file system module from node
 const fs = require("fs");
@@ -26,11 +27,42 @@ mongoose
 
 //middlewares
 app.use(cors());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+const { auth, isAdmin } = require("./Middleware/auth");
+
+// TODO : init a storage engine for multer
+const storage = multer.diskStorage({
+  destination: "./Uploads/",
+  filename: function (req, file, callBack) {
+    callBack(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+// TODO : upload function
+const upload = multer({
+  storage: storage,
+  limits: { fieldSize: 10 * 1024 * 1024 },
+});
 
 //routes
 readdirSync("./Routes").map((route) =>
   app.use("/api", require("./Routes/" + route))
+);
+
+app.post(
+  "/admin/category",
+
+  upload.single("Image"),
+  auth,
+
+  isAdmin,
+
+  (req, res) => {
+    console.log(req.files);
+  }
 );
 
 app.listen(process.env.PORT, () => {
