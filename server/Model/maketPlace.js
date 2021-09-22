2f4a2bcb1cf86abd9766b2e6731b013dacca74e4
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-09-22 11:15:14 +0530

    updated marketPlace model : added price entry

--- server/Model/maketPlace.js
+++ server/Model/maketPlace.js
@@ -1,28 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const { ObjectId } = Schema;
 
 const marketPlaceSchema = new Schema(
-	{
-		name: {
-			type: String,
-			trim: true,
-			minLength: ['3', 'Too Short'],
-			text: true,
-		},
-        description: {
-			type: String,
-			trim: true,
-			minLength: ['3', 'Too Short'],
-			text: true,
-		},
-		images: [Object],
-		recruiter: {
-			type: ObjectId,
-			ref: 'USER',
-		},
-	},
-	{ timestamps: true },
+  {
+    name: {
+      type: String,
+      trim: true,
+      minLength: ["3", "Too Short"],
+      text: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      minLength: ["3", "Too Short"],
+      text: true,
+    },
+    price: {
+      type: Number,
+    },
+    images: [Object],
+    recruiter: {
+      type: ObjectId,
+      ref: "USER",
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('MARKET', marketPlaceSchema);
+module.exports = mongoose.model("MARKET", marketPlaceSchema);
