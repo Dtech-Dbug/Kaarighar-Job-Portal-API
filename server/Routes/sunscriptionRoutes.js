6ba458163ad229205682163ff43916e15c834aea
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-09-24 11:10:39 +0530

    update subScription controllers

--- server/Routes/sunscriptionRoutes.js
+++ server/Routes/sunscriptionRoutes.js
@@ -1,5 +1,34 @@
 const express = require("express");
 
-router = express.Router();
+const router = express.Router();
+
+//middlewares
+const { auth, isAdmin } = require("../Middleware/auth");
+
+//create SUBMODEL
+
+router.post("/create-plan", auth, isAdmin, async (req, res) => {
+  try {
+    // do something
+  } catch (err) {
+    res.send("err.message");
+  }
+});
+
+router.get("get-plans", async (req, res) => {
+  try {
+    // do something
+  } catch (err) {
+    res.send("err.message");
+  }
+});
+
+router.get("get-plan/:id", auth, isAdmin, async (req, res) => {
+  try {
+    // do something
+  } catch (err) {
+    res.send("err.message");
+  }
+});
 
 module.exports = router;
