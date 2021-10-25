23916fe3e494196773a25fcb75122bc3b198ea49
ashishxcode <ashish.patel0903@gmail.com>
2021-10-25 13:18:27 +0530

    Fixed the url in user function in client side

--- client/src/functions/users.js
+++ client/src/functions/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
+const url = 'http://localhost:8000/api/';
 
-const url = '/api/';
 export const verifyUser = async (values) => {
 	return await axios.post(`${url}admin/verifyuser`, values, {
 		headers: {
