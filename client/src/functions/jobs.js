48dc8a55132da4da50b6cd076089b27e9c8c2fe6
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-11-17 01:08:41 +0530

    whitespace

--- client/src/functions/jobs.js
+++ client/src/functions/jobs.js
@@ -1,18 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url =  "http://localhost:8000/api/";
+const url = "http://localhost:8000/api/";
 export const getAllJobs = async () => {
-	return await axios.get(`${url}jobs`, {
-		headers: {
-			'auth-token': localStorage.getItem('token'),
-		},
-	});
+  return await axios.get(`${url}jobs`, {
+    headers: {
+      "auth-token": localStorage.getItem("token"),
+    },
+  });
 };
 
 export const getJobInfo = async (id) => {
-	return await axios.get(`${url}/job/${id}`, {
-		headers: {
-			'auth-token': localStorage.getItem('token'),
-		},
-	});
+  return await axios.get(`${url}/job/${id}`, {
+    headers: {
+      "auth-token": localStorage.getItem("token"),
+    },
+  });
 };
