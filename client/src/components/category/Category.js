ca4c3da6a0a5533b58172e08a63aafea3ea5d774
ashishxcode <ashish.patel0903@gmail.com>
2021-11-30 21:34:40 +0530

    Fixed the image upload feature in category

--- client/src/components/category/Category.js
+++ client/src/components/category/Category.js
@@ -1,85 +1,52 @@
-import React, { useState, useEffect } from "react";
-import CategoryForm from "./CategoryForm";
-import CategoryList from "./CategoryList";
+import React, { useState, useEffect } from 'react';
+import CategoryForm from './CategoryForm';
+import CategoryList from './CategoryList';
 import {
-  createCategory,
-  getAllCategories,
-  deleteCategory,
-  // editCategory,
-} from "../../functions/categories";
+	getAllCategories,
+	deleteCategory,
+	// editCategory,
+} from '../../functions/categories';
 
-import { RiLayout2Line } from "react-icons/ri";
+import { RiLayout2Line } from 'react-icons/ri';
 
 const Category = ({ match, history }) => {
-  const [formData, setFormData] = useState("");
-  const [file, setFile] = useState([]);
-  const [categoryList, setCategoryList] = useState([]);
+	const [categoryList, setCategoryList] = useState([]);
 
-  useEffect(() => {
-    fetchCategories();
-  }, []);
+	useEffect(() => {
+		fetchCategories();
+	}, []);
 
-  const fetchCategories = () =>
-    getAllCategories()
-      .then((categories) => {
-        setCategoryList(categories.data);
-      })
-      .catch((err) => alert(err.message));
+	const fetchCategories = () =>
+		getAllCategories()
+			.then((categories) => {
+				setCategoryList(categories.data);
+			})
+			.catch((err) => alert(err.message));
 
-  const handleCategoryChange = (e) => {
-    setFormData(e.target.value);
-  };
+	const handleCategoryDelete = (slug) => {
+		let confirm = window.confirm('Are you sure , you want to delete?');
+		if (confirm) {
+			deleteCategory(slug)
+				.then((res) => {
+					fetchCategories();
+				})
+				.catch((err) => alert('Error while deleting in frontend', err.message));
+		}
+	};
 
-  const handleCategoryFormSubmit = (e, values) => {
-    e.preventDefault();
-
-    let formValues = {
-      title: formData,
-      file: file,
-    };
-    console.log("DATA->", formValues);
-
-    //making req to BE to create category
-    createCategory(formValues)
-      .then((res) => {
-        alert("CATEGORY CREATED");
-      })
-      .catch((err) => console.log(err.message));
-  };
-
-  const handleCategoryDelete = (slug) => {
-    let confirm = window.confirm("Are you sure , you want to delete?");
-    if (confirm) {
-      deleteCategory(slug)
-        .then((res) => {
-          fetchCategories();
-        })
-        .catch((err) => alert("Error while deleting in frontend", err.message));
-    }
-  };
-
-  const handleFile = (e) => {
-    console.log("File->", e.target.files[0]);
-    setFile(e.target.files[0]);
-  };
-
-  return (
-    <>
-      <h1 className="flex items-center font-bold text-lg py-1.5">
-        <RiLayout2Line />
-        &nbsp;Category
-      </h1>
-      <CategoryForm
-        handleCategoryChange={handleCategoryChange}
-        handleCategoryFormSubmit={handleCategoryFormSubmit}
-        setFile={setFile}
-      />
-      <CategoryList
-        categoryList={categoryList}
-        handleCategoryDelete={handleCategoryDelete}
-      />
-    </>
-  );
+	return (
+		<>
+			<h1 className="flex items-center font-bold text-lg py-1.5">
+				<RiLayout2Line />
+				&nbsp;Category
+			</h1>
+			<CategoryForm />
+			<CategoryList
+				categoryList={categoryList}
+				handleCategoryDelete={handleCategoryDelete}
+			/>
+		</>
+	);
 };
 
 export default Category;
--- client/src/components/category/CategoryForm.js
+++ client/src/components/category/CategoryForm.js
@@ -1,53 +1,63 @@
-import React from "react";
-import Resizer from "react-image-file-resizer";
+import React, { useState } from 'react';
+import { Card, Button, message } from 'antd';
+import 'antd/dist/antd.css';
+import { createCategory } from '../../functions/categories';
 
-import { Card, Form, Button, Input, Upload, message } from "antd";
-import { InboxOutlined } from "@ant-design/icons";
-import "antd/dist/antd.css";
-const { Dragger } = Upload;
+const CategoryForm = () => {
+	const [newCategory, setNewCategory] = useState({
+		title: '',
+		file: '',
+	});
 
-const CategoryForm = ({
-  handleCategoryChange,
-  handleCategoryFormSubmit,
-  handleFile,
-  setFile,
-}) => {
-  let props = {
-    headers: {
-      "auth-token": localStorage.getItem("token"),
-    },
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const formData = new FormData();
+		formData.append('file', newCategory.file);
+		formData.append('title', newCategory.title);
 
-  return (
-    <Card className="w-full">
-      <form
-        action="/singleFile"
-        method="POST"
-        name="Category"
-        layout="vertical"
-        enctype="multipart/form-data"
-      >
-        <Input
-          placeholder="Enter Category Name."
-          className="p-2"
-          onChange={handleCategoryChange}
-        />
-        <input
-          name="image"
-          type="file"
-          onChange={(e) => setFile(e.target.files[0])}
-        ></input>
-        <Button
-          onClick={handleCategoryFormSubmit}
-          type="primary"
-          size="large"
-          className=" my-4"
-        >
-          Add Category
-        </Button>
-      </form>
-    </Card>
-  );
+		console.log(newCategory);
+
+		createCategory(formData).then((res) => {
+			if (res.status === 200) {
+				message.success('Category created successfully');
+				window.location.reload();
+			} else {
+				message.error('Category creation failed');
+			}
+		});
+	};
+
+	const handleChange = (e) => {
+		setNewCategory({ ...newCategory, [e.target.title]: e.target.value });
+	};
+
+	const handlefile = (e) => {
+		setNewCategory({ ...newCategory, file: e.target.files[0] });
+	};
+
+	return (
+		<Card className="w-full">
+			<form onSubmit={handleSubmit} method="post" enctype="multipart/form-data">
+				<input
+					placeholder="Enter Category Name."
+					className="w-full p-2 my-2 border-2"
+					type="text"
+					title="title"
+					onChange={handleChange}
+				/>
+				<input
+					type="file"
+					accept=".png, .jpg, .jpeg"
+					title="file"
+					className="w-full my-2"
+					onChange={handlefile}
+				></input>
+				<Button type="primary" htmlType="submit" size="large" className=" my-4">
+					Add Category
+				</Button>
+			</form>
+		</Card>
+	);
 };
 
 export default CategoryForm;
