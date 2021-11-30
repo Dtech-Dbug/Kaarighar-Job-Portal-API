685745dc55f4d2f47ca1b05c35982889ce3bedc2
ashishxcode <ashish.patel0903@gmail.com>
2021-11-30 21:31:20 +0530

    Added file filtter for image files

--- server/Helper/fileupload.js
+++ server/Helper/fileupload.js
@@ -2,18 +2,30 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads');
-    },
-    filename:  function (req, file, cb) { 
-        cb(null,new Date().toISOString().replace(/:/g,'-')+'-'+file.originalname);
-    }
+	destination: function (req, file, cb) {
+		cb(null, 'uploads');
+	},
+	filename: function (req, file, cb) {
+		cb(
+			null,
+			new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname
+		);
+	},
 });
 
+const fileFilter = (req, file, cb) => {
+	const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+	if (allowedFileTypes.includes(file.mimetype)) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
 
 const upload = multer({
-    storage: storage,
-    limits: { fieldSize: 10 * 1024 * 1024 },
+	storage: storage,
+	limits: { fieldSize: 10 * 1024 * 1024 },
+	fileFilter,
 });
 
-module.exports = {upload};
+module.exports = { upload };
