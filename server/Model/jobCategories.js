824d6a1b4dafb8ec7c274722cf6a3a5a3522377d
ashishxcode <ashish.patel0903@gmail.com>
2021-12-02 18:23:40 +0530

    Removed the image requirment tag

--- server/Model/jobCategories.js
+++ server/Model/jobCategories.js
@@ -1,43 +1,41 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
 //text field to be able to search the categories by text in the future
 
 const categoriesSchema = new Schema(
-  {
-    title: {
-      type: String,
-      trim: true,
-      minLength: ["3", "Too Short"],
-      text: true,
-    },
-    slug: {
-      type: String,
-      trim: true,
-      lowercase: true,
-      index: true,
-      text: true,
-    },
+	{
+		title: {
+			type: String,
+			trim: true,
+			minLength: ['3', 'Too Short'],
+			text: true,
+		},
+		slug: {
+			type: String,
+			trim: true,
+			lowercase: true,
+			index: true,
+			text: true,
+		},
 
-    images: {
-      fileName: {
-        type: String,
-        required: true,
-      },
-      filePath: {
-        type: String,
-        required: true,
-      },
-      fileType: {
-        type: String,
-      },
-      fileSize: {
-        type: String,
-      },
-    },
-  },
-  { timestamps: true }
+		images: {
+			fileName: {
+				type: String,
+			},
+			filePath: {
+				type: String,
+			},
+			fileType: {
+				type: String,
+			},
+			fileSize: {
+				type: String,
+			},
+		},
+	},
+	{ timestamps: true }
 );
 
-module.exports = mongoose.model("CATEGORIES", categoriesSchema);
+module.exports = mongoose.model('CATEGORIES', categoriesSchema);
